3b93a0af24dd723e2287a2a0133eba7ef70af005
Vivek <viveknarayanan15082@cse.ssn.edu.in>
2019-01-16 21:58:41 +0530

    doing_decent

--- isolata.java
+++ isolata.java
@@ -14,6 +14,64 @@ class isolata
 {
     public static void sop(String s){System.out.print(s);}
 
+    public static int getValue(String key)
+    {
+        int i,return_value=0;
+        for(i=0;i<key.length();i++)
+            return_value += key.charAt(i);
+
+        return return_value;
+    }
+
+    public static String rotate(int direction, String word, int rotate_value)   //-1 = left 1 = right
+    {
+        String return_value ="";
+        int i;
+        rotate_value = rotate_value % word.length();
+        if(rotate_value == 0)
+            return word;
+        /*
+        if(direction == -1)
+        {
+            for(i=rotate_value;i<word.length();i++)
+                return_value += word.charAt(i);
+            for(i=0;i<rotate_value;i++)
+                return_value += word.charAt(i);
+        }
+        else
+        {
+            for(i=word.length()-rotate_value;i<word.length();i++)
+                return_value += word.charAt(i);
+            for(i=0;i<word.length()-rotate_value;i++)
+                return_value += word.charAt(i);
+        }*/
+        //Above can be reduced
+        int startval;
+        if(direction == -1)
+            startval = rotate_value; 
+        else
+           startval = word.length()-rotate_value;
+        for(i=startval;i<word.length();i++)
+                return_value += word.charAt(i);
+        for(i=0;i<startval;i++)
+                return_value += word.charAt(i);
+
+        return return_value;
+    }
+
+    public static String caesar(int enc_dec, int Key, String text)  //1 for enc, -1 for dec
+    {
+        String return_value = "";
+        int i;
+        for(i=0;i<text.length();i++)
+        {
+            return_value += text.charAt(i) + enc_dec*Key;
+        }
+
+        return return_value;
+
+    }
+
 
     public static void decrypt()
     {
@@ -21,21 +79,23 @@ class isolata
         int keyval;
         File file;
         BufferedReader in;
+        String input_string="",st,decrypted_string=""; 
         Scanner sc = new Scanner(System.in);
         sop("\nEnter path : ");
         path = sc.nextLine();
         sop("\nEnter key : ");
         key = sc.nextLine();
-        keyval = getValue(key);                                         //TODO - Keyval function
+        keyval = getValue(key);                                         
         file = new File(path);
         try{
             in = new BufferedReader(new FileReader(file)); 
-            String input_string="",st,decrypted_string=""; 
+            
             int word_counter = 0;
             while ((st = in.readLine()) != null) 
             {
-                input_string += st;      
-                decrypted_string+=   cae
+                input_string += st;     
+                sop(st+ "\n"); 
+                decrypted_string+=  caesar(-1,keyval,rotate(-1,st,word_counter));
                 word_counter++;
 
             }   
@@ -49,9 +109,13 @@ class isolata
             sop(e+"\n");
         }
 
+        sop("\n"+decrypted_string);
+
         //decryption part
 
     }
+
+    
 
     public static void add(){}
     
@@ -78,11 +142,14 @@ class isolata
 
     public static void main(String[] args)
     {
-      
-        while(menu())
+        //sop(rotate(1,"Namaskaram",4)+"");
+        decrypt();
+        /*while(menu())
         {
 
-        }
+        }*/
+
+        
 
     }
 }
